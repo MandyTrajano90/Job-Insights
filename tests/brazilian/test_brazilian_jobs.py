d89c164496b1826b490fef87a65bd5b1cc768953
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-01 10:19:28 -0300

    fixing tests

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -4,19 +4,21 @@ from unittest.mock import MagicMock, patch
 
 def test_brazilian_jobs():
     test_data = [
-        {"titulo": "Engenheiro", "salario": "R$5000", "tipo": "Integral"},
-        {"titulo": "Programador", "salario": "R$9000", "tipo": "Meio período"},
+        {"titulo": "Dev-Front", "salario": "R$5000", "tipo": "trainee"},
+        {"titulo": "DevOps", "salario": "R$9000", "tipo": "trainee"},
     ]
     process_jobs_mock = MagicMock()
     process_jobs_mock.read.return_value = test_data
     process_jobs_mock.jobs_list = test_data
+
     with patch("src.pre_built.brazilian_jobs.ProcessJobs",
                return_value=process_jobs_mock,
                ):
         result = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
 
     expected_result = [
-        {"title": "Engenheiro", "salary": "R$5000", "type": "Integral"},
-        {"title": "Programador", "salary": "R$9000", "type": "Meio período"},
+        {"title": "Dev-Front", "salary": "R$5000", "type": "trainee"},
+        {"title": "DevOps", "salary": "R$9000", "type": "trainee"},
     ]
+
     assert result == expected_result
