f0c55bda06c76a8b79fe2527237b043a00c5ab60
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-01 09:05:59 -0300

    Third and Fourth Requirements - feats get_unique_jog_types and filter_by_multiple_criteria methods

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Union
 import csv
 
 
@@ -12,7 +12,24 @@ class ProcessJobs:
             self.jobs_list = [row for row in reader]
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        unique_job = set()
+        for row in self.jobs_list:
+            job_type = row["job_type"]
+            if job_type and job_type not in unique_job:
+                unique_job.add(job_type)
+        return list(unique_job)
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+            self,
+            jobs: List[Dict[str, str]],
+            filter_criteria: Dict[str, Union[str, List[str]]],
+            ) -> List[dict]:
+        if not isinstance(filter_criteria, dict):
+            raise TypeError("filter_by_multiple_criteria must be a dictionary")
+        filtered_jobs = []
+        for job in jobs:
+            if all(
+                job.get(key) in value for key, value in filter_criteria.items()
+            ):
+                filtered_jobs.append(job)
+        return filtered_jobs
