ed3762652661593c793afc65a91b27fecc41e43a
Mandytrajano90 <mandytrajano90@gmail.com>
2024-03-31 22:17:53 -0300

    First and Second Requirements - feats count ocurrences and read method

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    python_ocurrences = count_ocurrences("data/jobs.csv", "Python")
+    js_ocurrences = count_ocurrences("data/jobs.csv", "JavaScript")
+    assert python_ocurrences == 1639
+    assert js_ocurrences == 122
